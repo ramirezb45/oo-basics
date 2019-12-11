40a7eb5fb7f710996a96953d2c72eca599f3d74d
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-12-11 06:46:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,3 +10,17 @@ class Shoe
   end
 end
   
+  
+  class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
