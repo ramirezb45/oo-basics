2a336e6a3230fec181429a493fb82119a284ce26
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-12-11 06:32:13 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -42,3 +42,18 @@ end
 
 book = Book.new("Some Title")
 book.turn_page
+
+
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "flipping the page...wow you read fast!"
+  end
+  
+end
